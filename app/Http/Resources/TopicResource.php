585284fefb012b05825e2840ebd1ef92f12aa357
lgyer <lgy330395508@gmail.com>
2018-05-23 17:31:03 +0800

    Add topic model scope.

--- app/Http/Resources/TopicResource.php
+++ app/Http/Resources/TopicResource.php
@@ -3,9 +3,9 @@
 namespace App\Http\Resources;
 
 use App\Models\Topic;
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class TopicResource extends Resource
+class TopicResource extends ResourceCollection
 {
     /**
      * Transform the resource into an array.
@@ -15,19 +15,20 @@ class TopicResource extends Resource
      */
     public function toArray($request)
     {
-        return [
-            'id'                 => $this->id,
-            'title'              => $this->title,
-            'body'               => $this->body,
-            'user_id'            => (int) $this->user_id,
-            'category_id'        => (int) $this->category_id,
-            'reply_count'        => (int) $this->reply_count,
-            'view_count'         => (int) $this->view_count,
-            'last_reply_user_id' => (int) $this->last_reply_user_id,
-            'excerpt'            => $this->excerpt,
-            'slug'               => $this->slug,
-            'created_at'         => $this->created_at->toDateTimeString(),
-            'updated_at'         => $this->updated_at->toDateTimeString(),
-        ];
+        return $this->collection;
+        // return [
+        //     'id'                 => $this->id,
+        //     'title'              => $this->title,
+        //     'body'               => $this->body,
+        //     'user_id'            => (int) $this->user_id,
+        //     'category_id'        => (int) $this->category_id,
+        //     'reply_count'        => (int) $this->reply_count,
+        //     'view_count'         => (int) $this->view_count,
+        //     'last_reply_user_id' => (int) $this->last_reply_user_id,
+        //     'excerpt'            => $this->excerpt,
+        //     'slug'               => $this->slug,
+        //     'created_at'         => $this->created_at->toDateTimeString(),
+        //     'updated_at'         => $this->updated_at->toDateTimeString(),
+        // ];
     }
 }
